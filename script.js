9e1c89bf532c0a42d6cd2dae050bb48c570322ab
Jitendra-patel1 <125756903+Jitendra-patel1@users.noreply.github.com>
2023-08-13 22:29:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-function threeSum(arr, target) {
-// write your code here
+function threeSum(nums, target) {
+  const n = nums.length;
+    nums.sort((a, b) => a - b);
+    let ans = 0;
+    let diff = Number.MAX_VALUE;
+
+    for (let i = 0; i < n - 2; i++) {
+        let j = i + 1;
+        let k = n - 1;
+
+        while (j < k) {
+            const sum = nums[i] + nums[j] + nums[k];
+            if (Math.abs(target - sum) < diff) {
+                ans = sum;
+                diff = Math.abs(target - sum);
+            }
+
+            if (sum === target) {
+                return target;
+            } else if (sum > target) {
+                k--;
+            } else {
+                j++;
+            }
+        }
+    }
+    return ans;
+}
+
+
+
+
+
+
+
   
 }
 
-module.exports = threeSum;
